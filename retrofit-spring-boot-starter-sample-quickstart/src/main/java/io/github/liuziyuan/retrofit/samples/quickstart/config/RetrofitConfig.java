6617c8f28c7813ba21e1647e74c65dce43919fdc
liuziyuan <liuziyuan.room@gmail.com>
2022-03-11 18:01:38 +0800

    move @EnableRetrofit

--- retrofit-spring-boot-starter-sample-quickstart/src/main/java/io/github/liuziyuan/retrofit/samples/quickstart/config/RetrofitConfig.java
+++ retrofit-spring-boot-starter-sample-quickstart/src/main/java/io/github/liuziyuan/retrofit/samples/quickstart/config/RetrofitConfig.java
@@ -1,16 +1,18 @@
 package io.github.liuziyuan.retrofit.samples.quickstart.config;
 
 import io.github.liuziyuan.retrofit.RetrofitResourceDefinitionRegistry;
+import io.github.liuziyuan.retrofit.annotation.EnableRetrofit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author liuziyuan
  */
+@EnableRetrofit
 @Configuration
 public class RetrofitConfig {
     @Bean
-    public RetrofitResourceDefinitionRegistry retrofitResourceDefinitionRegistry() {
+    public static RetrofitResourceDefinitionRegistry retrofitResourceDefinitionRegistry() {
         return new RetrofitResourceDefinitionRegistry();
     }
 }
