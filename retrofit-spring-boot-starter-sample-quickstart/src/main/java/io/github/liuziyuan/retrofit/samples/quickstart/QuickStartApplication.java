7465155fd825f725ade75a77850c8cd42bae66a1
liuziyuan <liuziyuan.room@gmail.com>
2022-04-02 10:33:03 +0800

    add swagger-ui URL

--- retrofit-spring-boot-starter-sample-quickstart/src/main/java/io/github/liuziyuan/retrofit/samples/quickstart/QuickStartApplication.java
+++ retrofit-spring-boot-starter-sample-quickstart/src/main/java/io/github/liuziyuan/retrofit/samples/quickstart/QuickStartApplication.java
@@ -1,16 +1,22 @@
 package io.github.liuziyuan.retrofit.samples.quickstart;
 
 import io.github.liuziyuan.retrofit.annotation.EnableRetrofit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author liuziyuan
  */
+@Slf4j
 @EnableRetrofit
 @SpringBootApplication
 public class QuickStartApplication {
     public static void main(String[] args) {
-        SpringApplication.run(QuickStartApplication.class, args);
+        final ConfigurableApplicationContext run = SpringApplication.run(QuickStartApplication.class, args);
+        final ConfigurableEnvironment environment = run.getEnvironment();
+        log.info("swagger-ui URL: http://localhost:{}/swagger-ui/index.html", environment.getProperty("server.port"));
     }
 }
