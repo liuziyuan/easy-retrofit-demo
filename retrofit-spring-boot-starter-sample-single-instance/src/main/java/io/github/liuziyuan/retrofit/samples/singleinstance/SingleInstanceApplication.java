b0bc00176047e9774e8e166f77f2caa124a8f3f2
liuziyuan <liuziyuan.room@gmail.com>
2022-04-02 10:30:34 +0800

    upgrade to v0.0.17

--- retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/SingleInstanceApplication.java
+++ retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/SingleInstanceApplication.java
@@ -1,16 +1,22 @@
 package io.github.liuziyuan.retrofit.samples.singleinstance;
 
 import io.github.liuziyuan.retrofit.annotation.EnableRetrofit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author liuziyuan
  */
+@Slf4j
 @EnableRetrofit(basePackages = "io.github.liuziyuan.retrofit.samples.singleinstance.api")
 @SpringBootApplication
 public class SingleInstanceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SingleInstanceApplication.class, args);
+        final ConfigurableApplicationContext run = SpringApplication.run(SingleInstanceApplication.class, args);
+        final ConfigurableEnvironment environment = run.getEnvironment();
+        log.info("swagger-ui URL: http://localhost:{}/swagger-ui/index.html", environment.getProperty("server.port"));
     }
 }
