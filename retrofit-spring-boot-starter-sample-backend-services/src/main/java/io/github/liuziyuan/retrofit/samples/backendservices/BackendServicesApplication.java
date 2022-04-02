010b8013ed3ed11fd4713019611d18365579df85
liuziyuan <liuziyuan.room@gmail.com>
2022-04-02 10:40:24 +0800

    add swagger-ui

--- retrofit-spring-boot-starter-sample-backend-services/src/main/java/io/github/liuziyuan/retrofit/samples/backendservices/BackendServicesApplication.java
+++ retrofit-spring-boot-starter-sample-backend-services/src/main/java/io/github/liuziyuan/retrofit/samples/backendservices/BackendServicesApplication.java
@@ -1,14 +1,20 @@
 package io.github.liuziyuan.retrofit.samples.backendservices;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author liuziyuan
  */
+@Slf4j
 @SpringBootApplication
 public class BackendServicesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BackendServicesApplication.class, args);
+        final ConfigurableApplicationContext run = SpringApplication.run(BackendServicesApplication.class, args);
+        final ConfigurableEnvironment environment = run.getEnvironment();
+        log.info("swagger-ui URL: http://localhost:{}/swagger-ui/index.html", environment.getProperty("server.port"));
     }
 }
