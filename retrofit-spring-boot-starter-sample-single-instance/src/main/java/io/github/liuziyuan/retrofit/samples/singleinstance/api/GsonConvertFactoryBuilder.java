19b8c4db6c23e22212825e4b6afed6b9fe734306
liuziyuan <liuziyuan.room@gmail.com>
2024-02-21 15:21:09 +0800

    modify

--- retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/api/GsonConvertFactoryBuilder.java
+++ retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/api/GsonConvertFactoryBuilder.java
@@ -1,15 +1,24 @@
 package io.github.liuziyuan.retrofit.samples.singleinstance.api;
 
 import io.github.liuziyuan.retrofit.extension.BaseConverterFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import retrofit2.Converter;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author liuziyuan
  */
+@Slf4j
+@Component
 public class GsonConvertFactoryBuilder extends BaseConverterFactoryBuilder {
+
+    @Value("${okhttpclient.timeout}")
+    private int timeout;
     @Override
     public Converter.Factory buildConverterFactory() {
+        log.info("time out: {}", timeout);
         return GsonConverterFactory.create();
     }
 }
--- retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/api/MyRetrofitInterceptor.java
+++ retrofit-spring-boot-starter-sample-single-instance/src/main/java/io/github/liuziyuan/retrofit/samples/singleinstance/api/MyRetrofitInterceptor.java
@@ -3,11 +3,13 @@ package io.github.liuziyuan.retrofit.samples.singleinstance.api;
 import io.github.liuziyuan.retrofit.RetrofitResourceContext;
 import io.github.liuziyuan.retrofit.extension.BaseInterceptor;
 import io.github.liuziyuan.retrofit.resource.RetrofitServiceBean;
+import io.github.liuziyuan.retrofit.samples.singleinstance.controller.HelloService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import retrofit2.Invocation;
 
@@ -25,12 +27,21 @@ public class MyRetrofitInterceptor extends BaseInterceptor {
     @Autowired
     private RetrofitResourceContext context;
 
+    @Autowired
+    private HelloService helloService;
+
+
+    @Value("${okhttpclient.timeout}")
+    private int timeout;
+
     @SneakyThrows
     @Override
     protected Response executeIntercept(Chain chain) {
         Request request = chain.request();
         String clazzName = Objects.requireNonNull(request.tag(Invocation.class)).method().getDeclaringClass().getName();
         final RetrofitServiceBean currentServiceBean = context.getRetrofitServiceBean(clazzName);
+        log.info("HelloService: {}", helloService.hello("retrofit"));
+        log.info("time out: {}", timeout);
         // TODO if you need
         return chain.proceed(request);
     }
