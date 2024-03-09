94ddd53b6f1c6e2acef64e7f4da9db128a92f28d
liuziyuan <liuziyuan.room@gmail.com>
2024-03-09 14:41:46 +0800

    modify

--- retrofit-spring-boot-reactor-starter-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/web/samples/RetrofitSpringBootReactorApplication.java
+++ retrofit-spring-boot-reactor-starter-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/web/samples/RetrofitSpringBootReactorApplication.java
@@ -11,7 +11,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
  * @author liuziyuan
  */
 @Slf4j
-@EnableRetrofit(basePackages = "io.github.liuziyuan.retrofit.samples.awesome.api")
+@EnableRetrofit(basePackages = "io.github.liuziyuan.retrofit.spring.boot.web.samples.api")
 @SpringBootApplication
 public class RetrofitSpringBootReactorApplication {
     public static void main(String[] args) {
