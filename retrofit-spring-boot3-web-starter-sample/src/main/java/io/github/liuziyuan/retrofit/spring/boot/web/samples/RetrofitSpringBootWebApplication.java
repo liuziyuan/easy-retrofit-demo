cf08c91fae9cb7929031c9f2a4095dfc7a4024d3
liuziyuan <liuziyuan.room@gmail.com>
2024-03-11 15:43:56 +0800

    add spring boot3 web samples

---
+++ retrofit-spring-boot3-web-starter-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/web/samples/RetrofitSpringBootWebApplication.java
@@ -0,0 +1,35 @@
+package io.github.liuziyuan.retrofit.spring.boot.web.samples;
+
+import io.github.liuziyuan.retrofit.core.RetrofitBuilderExtension;
+import io.github.liuziyuan.retrofit.spring.boot.EnableRetrofit;
+import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @author liuziyuan
+ */
+@Slf4j
+@EnableRetrofit(basePackages = "io.github.liuziyuan.retrofit.spring.boot.web.samples.api")
+@SpringBootApplication
+public class RetrofitSpringBootWebApplication {
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext run = SpringApplication.run(RetrofitSpringBootWebApplication.class, args);
+        String[] beanNamesForType = run.getBeanNamesForType(RetrofitBuilderExtension.class);
+        log.info("beanNamesForType: " + Arrays.stream(beanNamesForType).collect(Collectors.joining(",")));
+//        ConfigurationBuilder configuration;
+//        configuration = new ConfigurationBuilder().forPackages("");
+//        Reflections reflections = new Reflections(configuration);
+
+//        final ConfigurableEnvironment environment = run.getEnvironment();
+//        log.info("swagger-ui URL: http://localhost:{}/swagger-ui/index.html", environment.getProperty("server.port"));
+    }
+}
---
+++ retrofit-spring-boot3-web-starter-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/web/samples/domain/HelloBean.java
@@ -0,0 +1,11 @@
+package io.github.liuziyuan.retrofit.spring.boot.web.samples.domain;
+
+import lombok.Data;
+
+/**
+ * @author liuziyuan
+ */
+@Data
+public class HelloBean {
+    private String message;
+}
