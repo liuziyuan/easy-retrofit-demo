543bfb120cb4f0dc7046fe3d097ec18d5d0ce607
liuziyuan <liuziyuan.room@gmail.com>
2024-08-22 17:05:01 +0800

    aliyun 相关

--- retrofit-spring-boot-aliyun-api-gateway-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/aliyun/api/gateway/samples/api/TestApi.java
+++ retrofit-spring-boot-aliyun-api-gateway-sample/src/main/java/io/github/liuziyuan/retrofit/spring/boot/aliyun/api/gateway/samples/api/TestApi.java
@@ -1,5 +1,6 @@
 package io.github.liuziyuan.retrofit.spring.boot.aliyun.api.gateway.samples.api;
 
+import io.github.liuziyuan.retrofit.spring.boot.aliyun.api.gateway.api.AliyunApiGateWay;
 import retrofit2.http.GET;
 
 public interface TestApi extends AliyunApiGateWay {
