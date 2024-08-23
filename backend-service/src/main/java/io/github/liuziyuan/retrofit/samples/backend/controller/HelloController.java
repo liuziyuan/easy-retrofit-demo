6e148835893b1cfb7e3f5d0aded3a165cc1b883c
liuziyuan <liuziyuan.room@gmail.com>
2024-08-23 13:03:14 +0800

    add v2 api

--- backend-service/src/main/java/io/github/liuziyuan/retrofit/samples/backend/controller/HelloController.java
+++ backend-service/src/main/java/io/github/liuziyuan/retrofit/samples/backend/controller/HelloController.java
@@ -7,24 +7,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Random;
 
 @RestController
-@RequestMapping("/backend/v1/hello")
+@RequestMapping("/backend")
 public class HelloController {
 
-    @GetMapping("/{message}")
+    @GetMapping("/v1/hello/{message}")
     public ResponseEntity<HelloBean> helloPath(@PathVariable String message) {
         HelloBean helloBean = new HelloBean();
         helloBean.setMessage(message + new Random().nextInt());
         return ResponseEntity.ok(helloBean);
     }
 
-    @GetMapping("")
+    @GetMapping("/v2/hello/{message}")
+    public ResponseEntity<HelloBean> helloPath2(@PathVariable String message) {
+        HelloBean helloBean = new HelloBean();
+        helloBean.setMessage(message + new Random().nextInt());
+        return ResponseEntity.ok(helloBean);
+    }
+
+    @GetMapping("/v1/hello")
     public ResponseEntity<HelloBean> helloParam(@RequestParam String message) {
         HelloBean helloBean = new HelloBean();
         helloBean.setMessage(message + new Random().nextInt());
         return ResponseEntity.ok(helloBean);
     }
 
-    @PostMapping("")
+    @PostMapping("/v1/hello")
     public ResponseEntity<HelloBean> helloBody(@RequestBody HelloBean helloBean) {
         helloBean.setMessage(helloBean.getMessage() + new Random().nextInt());
         return ResponseEntity.ok(helloBean);
