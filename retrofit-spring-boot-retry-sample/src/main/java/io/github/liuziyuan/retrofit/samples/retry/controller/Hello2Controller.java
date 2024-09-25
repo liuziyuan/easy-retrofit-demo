91b5e954f28b103faabe952dbb1be10123bacd53
liuziyuan <liuziyuan.room@gmail.com>
2024-09-25 14:29:56 +0800

    modify

--- retrofit-spring-boot-retry-sample/src/main/java/io/github/liuziyuan/retrofit/samples/retry/controller/Hello2Controller.java
+++ retrofit-spring-boot-retry-sample/src/main/java/io/github/liuziyuan/retrofit/samples/retry/controller/Hello2Controller.java
@@ -1,5 +1,6 @@
 package io.github.liuziyuan.retrofit.samples.retry.controller;
 
+import io.github.easyretrofit.core.exception.RetrofitExtensionException;
 import io.github.liuziyuan.retrofit.samples.retry.HelloBean;
 import io.github.liuziyuan.retrofit.samples.retry.api.HelloApi;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.View;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -18,15 +20,26 @@ import java.io.IOException;
  */
 @Slf4j
 @RestController
-@RequestMapping("/v2/hello")
 public class Hello2Controller {
 
     @Autowired
     private HelloApi helloApi;
+    @Autowired
+    private View error;
 
 
-    @GetMapping("/{message}")
+    @GetMapping("/v2/hello/{message}")
     public Mono<HelloBean> hello(@PathVariable String message) throws IOException {
         return Mono.fromCallable(helloApi::error400);
+    }
+
+    @GetMapping("/v3/hello/{message}")
+    public Mono<HelloBean> helloMono400(@PathVariable String message) throws IOException {
+        return helloApi.helloMono400().onErrorResume(error -> {
+            log.info("error400 fallback {}", error.getMessage());
+            HelloBean helloBean = new HelloBean();
+            helloBean.setMessage("mono error400 fallback");
+            return Mono.just(helloBean);
+        });
     }
 }
